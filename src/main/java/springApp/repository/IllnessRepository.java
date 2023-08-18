a116fa267766485e8e78379dab0bf08a6f656b02
MOBelyaev <BelyaevMWork@yandex.ru>
2023-08-18 15:30:58 +0300

    Test commit

--- src/main/java/springApp/repository/IllnessRepository.java
+++ src/main/java/springApp/repository/IllnessRepository.java
@@ -1,6 +1,7 @@
 package springApp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import springApp.dto.IllnessDto;
 import springApp.model.Illness;
 
 public interface IllnessRepository extends JpaRepository<Illness, String> {
